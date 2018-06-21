8a09cc36041080205359a29368affd21330d1a51
Manuel Bachl <m.bachl@finndrop.de>
2018-06-21 22:32:09 +0200

    add bootstrap, jquery, angled-edges and popper.js

--- src/app/index.js
+++ src/app/index.js
@@ -2,9 +2,33 @@
  * Application entry point
  */
 
-// Load application styles
+import $ from 'jquery';
+
+import GitHub from 'github-api';
+
 import 'styles/index.scss';
 
-// ================================
-// START YOUR APP HERE
-// ================================
+// unauthenticated client
+const github = new GitHub();
+console.log(github);
+/*let gist = gh.getGist(); // not a gist yet
+gist.create({
+    public: true,
+    description: 'My first gist',
+    files: {
+        "file1.txt": {
+            content: "Aren't gists great!"
+        }
+    }
+}).then(function({data}) {
+    // Promises!
+    let createdGist = data;
+    return gist.read();
+}).then(function({data}) {
+    let retrievedGist = data;
+    // do interesting things
+});*/
+
+$(document).ready(function() {
+
+});
